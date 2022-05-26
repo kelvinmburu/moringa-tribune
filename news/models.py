2c563cf4382fc62635d265c821229438830a50fa
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-26 11:12:41 +0300

    build:created superuser admin accounts

--- news/models.py
+++ news/models.py
@@ -9,6 +9,9 @@ class Editor(models.Model):
     def __str__(self):
         return self.first_name
     
+    def save_editor(self):
+        self.save()
+    
     class Meta:
         ordering = ['first_name']
         
@@ -21,4 +24,8 @@ class tags(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
-    editor = models.ForeignKey(Editor)
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    
