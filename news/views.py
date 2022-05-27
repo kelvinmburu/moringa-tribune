e3abbb87dc94883ff7cd065b7c966ca62a77bb6e
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-27 09:42:41 +0300

    build: created media settings

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 import datetime as dt
-from django.http  import Http404
+from django.http  import HttpResponse, Http404
 from django.shortcuts import render, redirect
 from .models import Article
 
@@ -8,6 +8,22 @@ def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
     return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+
+def past_days_news(request,past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_today)
+    
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date":date,"news":news})
 
 
 def search_results(request):
--- tribune/settings.py
+++ tribune/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -131,3 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Default path for media uploads
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
